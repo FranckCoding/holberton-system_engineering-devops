02d56c390aa2e7a8b53072bf001c1b857b9d3e3e
Falendrac <leger_franck@hotmail.fr>
2022-05-16 12:21:25 +0200

    Add task 3 to 0x15.

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+'''
+Records all tasks that are owned by this employee
+'''
+
+
+import requests
+import json
+
+url = "https://jsonplaceholder.typicode.com/users"
+
+
+def create_dictionnary(users):
+    '''
+    create a dictionnary with the api request
+    '''
+    dictionnary = {}
+    for user in users.json():
+        user_tasks = requests.get("{}/{}/todos".format(
+            url, user['id'])).json()
+        if user_tasks:
+            dictionnary[str(user['id'])] = []
+            for value in user_tasks:
+                dictionnary[str(user['id'])].append(
+                    {"username": user["username"],
+                     "task": value['title'],
+                     "completed": value['completed']}
+                    )
+
+    return dictionnary
+
+
+def main():
+    '''
+    main function that not be executable if not main
+    '''
+    users = requests.get("{}".format(url))
+
+    if users:
+        dictionnary = create_dictionnary(users)
+
+        json_object = json.dumps(dictionnary)
+
+        with open('todo_all_employees.json', mode='w') as json_file:
+            json_file.write(json_object)
+
+
+if __name__ == '__main__':
+    main()
