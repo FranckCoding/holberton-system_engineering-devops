7838cb2f345357d5e8586fdfe54d74557002c683
Falendrac <leger_franck@hotmail.fr>
2022-05-31 10:10:13 +0200

    Add task 0 to 0x16.

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Task 0 - Use Reddit API with Python
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API and returns
+    the number of subscribers (not active users, total subscribers)
+    for a given subreddit. If an invalid subreddit is given,
+    the function should return 0
+    """
+    response = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json",
+                            headers= {'User-agent': 'Wololo'})
+    if response is None:
+        return 0
+
+    response_data = response.json()['data']
+
+    return response_data['subscribers']
